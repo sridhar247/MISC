7cc0100decfac44bdb1a876e8d0323617f771f6c
sridhar247 <sridhar247@gmail.com>
2025-03-10 22:41:57 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,11 +7,11 @@ for i in {1..7}; do
     SARFILE="/var/log/sa/sa$(date -d "$i days ago" +%d)"
 
     if [ -f "$SARFILE" ]; then
-        MEM_DATA=$(sar -r -f "$SARFILE" | awk 'NR>3 && $1 ~ /^[0-9]/ {print 100*($4/($2+$4))}')
+        MEM_DATA=$(sar -r -f "$SARFILE" | awk 'NR>3 && $1 ~ /^[0-9]/ {print ($3/$2)*100}')
 
         MEM_AVG=$(echo "$MEM_DATA" | awk '{sum+=$1; cnt++} END {if(cnt>0) printf "%.2f", sum/cnt; else print "N/A"}')
-        MEM_MAX=$(echo "$MEM_DATA" | sort -nr | head -1)
-        MEM_MIN=$(echo "$MEM_DATA" | sort -n | head -1)
+        MEM_MAX=$(echo "$MEM_DATA" | sort -nr | head -1 | xargs printf "%.2f")
+        MEM_MIN=$(echo "$MEM_DATA" | sort -n | head -1 | xargs printf "%.2f")
 
         echo -e "$DATE\t$MEM_AVG%\t\t$MEM_MAX%\t\t$MEM_MIN%"
     else
