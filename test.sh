a9d6f8c2b022cfd6076a99866422e8b3787bbc4f
sridhar247 <sridhar247@gmail.com>
2025-03-10 23:08:06 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,14 +7,16 @@ for i in {1..7}; do
     SARFILE="/var/log/sa/sa$(date -d "$i days ago" +%d)"
 
     if [ -f "$SARFILE" ]; then
-        MEM_USAGE=$(sar -r -f "$SARFILE" | awk '/^[0-9]/ {printf "%.2f\n", ($3/$2)*100}')
+        MEM_USAGE=$(sar -r -f "$SARFILE" | awk '/^[0-9]/ && $2>0 {printf "%.2f\n", ($3/$2)*100}')
 
-        AVG_MEM=$(echo "$MEM_USAGE" | awk '{sum+=$1; count++} END {if(count>0) printf "%.2f", sum/count; else print "N/A"}')
-        MAX_MEM=$(echo "$MEM_USAGE" | sort -nr | head -1)
-        MIN_MEM=$(echo "$MEM_USAGE" | sort -n | head -1)
+        # Check if MEM_USAGE is not empty
+        if [ -n "$MEM_USAGE" ]; then
+            AVG_MEM=$(echo "$MEM_USAGE" | awk '{sum+=$1; count++} END {printf "%.2f", sum/count}')
+            MAX_MEM=$(echo "$MEM_USAGE" | sort -nr | head -1)
+            MIN_MEM=$(echo "$MEM_USAGE" | sort -n | head -1)
 
-        echo -e "$DATE\t$AVG_MEM%\t\t$MAX_MEM%\t\t$MIN_MEM%"
-    else
-        echo -e "$DATE\tNo Data\t\tNo Data\t\tNo Data"
-    fi
+            echo -e "$DATE\t$AVG_MEM%\t\t$MAX_MEM%\t\t$MIN_MEM%"
+        else
+            echo -e "$DATE\tNo Data\t\tNo Data\t\tNo Data"
+        fi
 done
