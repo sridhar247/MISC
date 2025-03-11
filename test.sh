aaeaa26cee94b856f2cbd5b8e330055e93a10543
sridhar247 <sridhar247@gmail.com>
2025-03-10 23:14:49 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-echo -e "Date\t\tAverage CPU Utilization (%)"
+echo -e "Date\t\tAvg CPU%"
 
 for i in {1..7}; do
     DATE=$(date -d "$i days ago" +"%Y-%m-%d")
     SARFILE="/var/log/sa/sa$(date -d "$i days ago" +%d)"
 
     if [ -f "$SARFILE" ]; then
-        AVG_CPU=$(sar -u -f "$SARFILE" | \
-                  awk '/^[0-9]/ {usage+=(100 - $8); count++} END {if(count>0) printf "%.2f", usage/count; else print "N/A"}')
-        echo -e "$DATE\t$AVG_CPU%"
+        CPU_DATA=$(sar -u -f "$SARFILE" | awk '/^[0-9]/ {print 100 - $8}')
+
+        if [ -n "$CPU_DATA" ]; then
+            AVG_CPU=$(echo "$CPU_DATA" | awk '{sum+=$1; cnt++} END {if(cnt>0) printf "%.2f", sum/cnt; else print "N/A"}')
+            echo -e "$DATE\t$AVG_CPU%"
+        else
+            echo -e "$DATE\tNo Valid Data"
+        fi
     else
         echo -e "$DATE\tNo Data"
     fi
